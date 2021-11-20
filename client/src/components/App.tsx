ced2972dd744edd042196bc7b9d400e709d3619e
SmirnovDmitrii <smirnovdmitriis@ya.ru>
2021-11-20 15:13:45 +0400

    init project base file

---
+++ client/src/components/App.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+type AppProps = {
+
+}
+
+const App: React.FC<AppProps> = (props: AppProps) => {
+  return (
+    <>
+      <div>Yep-yep</div>
+    </>
+  );
+};
+
+export default App;
+
+App.displayName = 'App';
+
